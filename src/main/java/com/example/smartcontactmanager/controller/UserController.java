073f88570e3443ff9c33f51ecdf0791e01312036
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2022-09-16 11:58:44 +0530

    make responsive design of add contact

--- src/main/java/com/example/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/example/smartcontactmanager/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.smartcontactmanager.controller;
 
 import com.example.smartcontactmanager.dao.UserRepository;
+import com.example.smartcontactmanager.entities.Contact;
 import com.example.smartcontactmanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -17,18 +19,27 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/dashboard")
-    public String dashboard(Model model , Principal principal)
-    {
+    @ModelAttribute
+    public void addCommonDataToModal(Model model , Principal principal){
         /* here Principal is a java security package class in with help of this class object we can get user name after its login*/
         String name = principal.getName();
         // getting user details from the database.
         User user = userRepository.getUserByUserName(name);
-
-        model.addAttribute("title","Dashboard - Smart Contact Manager");
         model.addAttribute("user",user);
+    }
 
-
+    @GetMapping("/dashboard")
+    public String dashboard(Model model , Principal principal)
+    {
+        model.addAttribute("title","Dashboard - Smart Contact Manager");
         return "user/dashboard";
+    }
+
+    @GetMapping("/add-contact")
+    public String openAddFormPage(Model model , Principal principal)
+    {
+        model.addAttribute("title","Add Contact - Smart Contact Manager");
+        model.addAttribute("contact",new Contact());
+        return "user/add_contact_form";
     }
 }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,1 +1,21 @@
 console.log("This is text come from script file");
+
+let backdrop = document.querySelector(".backdrop");
+let side_bar = document.querySelector(".side-bar");
+let main_content = document.querySelector(".main-content");
+let menu_bar = document.querySelector(".menu-bar");
+
+
+menu_bar.addEventListener("click",()=>{
+    console.log("menu-bar");
+    backdrop.style.display="block";
+    side_bar.style.display="block";
+    main_content.style.display="none";
+});
+
+backdrop.addEventListener("click",()=>{
+    console.log("backdrop");
+    backdrop.style.display="none";
+    side_bar.style.display="none";
+    main_content.style.display="block";
+})
