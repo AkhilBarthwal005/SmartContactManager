a34d664ae235e71a3e54eef9ad696ba441310f96
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2022-09-16 17:34:24 +0530

    profile picture uploaded module is done

--- src/main/java/com/example/smartcontactmanager/controller/UserController.java
+++ src/main/java/com/example/smartcontactmanager/controller/UserController.java
@@ -5,14 +5,18 @@ import com.example.smartcontactmanager.dao.UserRepository;
 import com.example.smartcontactmanager.entities.Contact;
 import com.example.smartcontactmanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.annotation.MultipartConfig;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 
 @Controller
@@ -49,15 +53,33 @@ public class UserController {
     }
 
     @PostMapping("/process-add-contact")
-    public String saveContactDetails(@ModelAttribute("contact") Contact contact,Model model, Principal principal)
+    public String saveContactDetails(@ModelAttribute("contact") Contact contact, @RequestParam("profilePicture") MultipartFile profile, Model model, Principal principal)
     {
-        String name = principal.getName();
-        User user = userRepository.getUserByUserName(name);
-        System.out.println(contact);
-        contact.setUser(user);
-        user.getContacts().add(contact);
-        contactRepository.save(contact);
-        model.addAttribute("contact",new Contact());
+        try{
+            String name = principal.getName();
+            User user = userRepository.getUserByUserName(name);
+            System.out.println(contact);
+
+            if(profile.isEmpty()){
+                System.out.println("file is empty");
+                contact.setImage("default.png");
+            }
+            else{
+                File file = new ClassPathResource("static/images/profile").getFile();
+                Path path = Paths.get(file.getAbsolutePath() + File.separator +contact.getNickName()+"_"+ profile.getOriginalFilename());
+                contact.setImage(contact.getCId()+"_"+ profile.getOriginalFilename());
+                Files.copy(profile.getInputStream(),path, StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("File uploaded");
+            }
+
+            contact.setUser(user);
+            user.getContacts().add(contact);
+            contactRepository.save(contact);
+            model.addAttribute("contact",new Contact());
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
         return "user/add_contact_form";
     }
 
